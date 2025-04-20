fe44adfe736d742ee2d3f05b9c2bfb995c17c65b
_smriti_rai_ <140977626+Smritirai005@users.noreply.github.com>
2025-04-21 00:03:56 +0530

    added components

--- app/_components/Footer.jsx
+++ app/_components/Footer.jsx
@@ -1,5 +1,5 @@
 "use client"
-import { FaLinkedin } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
