b58691c8c7642185be24537d4e4eb0a755e15d58
_smriti_rai_ <140977626+Smritirai005@users.noreply.github.com>
2025-04-20 22:11:56 +0530

    connected the backend

---
+++ configs/db.jsx
@@ -0,0 +1,6 @@
+
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+
+const sql = neon(process.env.NEXT_PUBLIC_DB_CONNECTION_STRING); // make sure this is defined
+export const db = drizzle(sql);
---
+++ drizzle.config.js
@@ -0,0 +1,8 @@
+// drizzle.config.js (testing only)
+export default {
+  schema: "./configs/Schema.jsx",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: "postgresql://neondb_owner:npg_rRXGZ67TIjES@ep-morning-boat-a5jc9zci-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require",
+  },
+};
